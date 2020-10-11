7cded0662c659a0468c8592a33b02bad031e8248
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-10-12 01:41:50 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-//Init middleware
+//  Init middleware
 app.use(express.json({ extended: false }));
-
 app.get('/', (req, res) => res.send('Api is running'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`server started on port ${5000}`));
