39fd31f991522bf9045f2609ceba1e0f0e76e0d8
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-10-13 01:06:28 +0530

    update the server.js

--- server.js
+++ server.js
@@ -1,12 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth-route');
+const HttpError = require('./models/http-error');
+
+// db connection
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true })
+  .then(() => {
+    console.log('DB connected!!!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.use(cors());
 
-//  Init middleware
-app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('Api is running'));
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server started on port ${5000}`));
+// cors error handler
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+} else {
+  app.use(cors());
+}
+// routes middleware
+app.use('/api/auth', authRoutes);
+
+// Error Handler
+app.use(() => {
+  const error = new HttpError('page not found!', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    // when headers already sent, we can't output a error. because response already sent. so just return next
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ error: error.message || 'An unknown error occurred !!' });
+  return next();
+});
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+});
