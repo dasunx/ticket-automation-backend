a867fb72ccde87662247ec572e6e92f23f2a7a8e
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-10-18 00:16:37 +0530

    selects msg field from the response array

--- validators/index.js
+++ validators/index.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const runValidation = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ error: errors.array()[0] });
+        return res.status(422).json({ error: errors.array()[0].msg });
     }
     return next();
 };
